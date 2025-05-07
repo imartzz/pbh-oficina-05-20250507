78a1af8dc88d5a91a7cf14a744e4d6c210e5f794
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-05-07 06:32:01 +0000

    JSONServer

--- atividade/app.js
+++ atividade/app.js
@@ -17,7 +17,7 @@
  * 4) json-server --watch db/db.json --port 3333
 */
 
-const url = "https://9184a615-58dc-476b-9b6b-37318556498e-00-2bnic1duw2m7u.picard.replit.dev/";
+const url = "https://9184a615-58dc-476b-9b6b-37318556498e-00-2bnic1duw2m7u.picard.replit.dev";
 const urlCidades = url + "/cidades"; 
 console.log(urlCidades);
 let cidades = [];
